385c8a634f9beaa203ab1d988f06d85a1bd6d46c
Angelos Chalaris <chalarangelo@gmail.com>
2017-07-22 15:12:54 +0300

    Comments lesson completed
    Everything works, it even ends up in a completion state after 30 seconds.

--- 0_comments.js
+++ 0_comments.js
@@ -5,12 +5,21 @@ document.addEventListener('DOMContentLoaded', function(event){
   }
   else {
     user = JSON.parse(localStorage.getItem('learn-js-user'));
+    if (user['videos'] == 'yes')
+      document.getElementsByTagName('style')[0].innerHTML += "div.videos-further{ display: block;}"
+    if (user['desiredJsKnowledge'] == 'full')
+      document.getElementsByTagName('style')[0].innerHTML += "div.js-further{ display: block;}"
   }
   window.setTimeout(function(){
     if(user['comments'] == 'available' || user['comments'] == 'restricted')
       user['comments'] = 'seen';
     localStorage.setItem('learn-js-user',JSON.stringify(user));
   },5000);
+  window.setTimeout(function(){
+      user['comments'] = 'completed';
+    localStorage.setItem('learn-js-user',JSON.stringify(user));
+  },30000);
+  document.getElementsByTagName('style')[0].innerHTML += '.custom{background: red;}';
 });
 
 user = {};
